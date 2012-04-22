30bb86d8e95d9c0d2d30246ac7a08b059421fe67
sergioandreshc <sergioandreshc@live.com>
2012-04-22 09:22:11 -0700

    Implementation of the first five points of test

--- StringCalculator/src/app/AppLlauncher.java
+++ StringCalculator/src/app/AppLlauncher.java
@@ -1,13 +1,70 @@
 package app;
 
 public class AppLlauncher {
+	
 
 	/**
+	 * Launches the application creating an StringCalculator object and using its methods to 
+	 * calculate some operations with strings  
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// Test of version one
+		try{test(null, 9);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		try{test("", 9);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		test("23,20", 1);
+		test("2 f3,20i", 1);
+		test("i", 1);
+		// Test of version two
+		test("", 2);
+		test("23,20", 2);
+		test("23,20,4", 2);
+		test("i 2,2,3e,,i9,", 2);
+		// Test of version tree
+		test("", 3);
+		test("23\n20,12", 3);
+		try{test("23,\n204", 3);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		try{test("23\n,204", 3);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		try{test("23\n\n204", 3);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		try{test("23,,204", 3);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		try{test("23,,,,,,,,,,,,,,,,,,,204", 3);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		// Test of version four 
+		test("", 4);
+		test("//,\n23,20,2", 4);
+		try{test("//,\n23,,204", 4);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		try{test("//,\n23,204,", 4);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		try{test("//,\n,23,204", 4);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		try{test("//,\n23,2 04", 4);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		try{test("//,\n23,2f04", 4);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		try{test("//,\n23,204,-45", 4);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		try{test("//t\n23,204,45", 4);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		test("//t\n2t4t6t8", 4);
+		// Test of version five 
+		test("//t\n2t4t6t8", 5);
+		try{test("//t\n2t4t-6t8", 5);}
+		catch (Exception e) {System.out.println(e.getMessage());}
+		
+		
 	}
 
+	public static void test(final String toAdd, int version){
+		System.out.println();
+		System.out.println("Call StringCalculator.add function with arguments: {"+toAdd+"} and {"+version+"}");
+		System.out.println(StringCalculator.add(toAdd,version));
+	}
 }
--- StringCalculator/src/app/StringCalculator.java
+++ StringCalculator/src/app/StringCalculator.java
@@ -1,5 +1,117 @@
 package app;
 
+/**
+ * StringCalculator is a Utilitarian class that which finds decimal numbers on a string and
+ * add them
+ * @author sergioamdreshc
+ *
+ */
 public class StringCalculator {
 
+	/**
+	 * extract integer numbers from the given string and adds them, the behavior of this method
+	 * depends on the <b>version</b> parameter
+	 * @param toAdd the string from which the numbers will be extracted this string have to match
+	 * the structure of the mode.
+	 * @param version integer value that chooses the version in which the method will work the grater
+	 * the version the more functionalities it has  
+	 * <ol>
+	 * <li>simple String calculator It takes 0, 1 or 2 numbers, and will return their sum (for an 
+	 * empty string it will return 0) for example “” or “1” or “1,2” if more tan 2 numbers are provided
+	 * they are skipped if other characters different to decimals or, are on the string they are skipped</li>
+	 * <li>on going...</li>
+	 * </ol> 
+	 * @return the sum of the integers on the string
+	 */
+	public static int add(String toAdd, int version) {
+		int returnValue = 0;
+		String[] numbers;
+		if(toAdd == null){
+			throw new IllegalArgumentException("Null values are not allowed");
+		}
+		
+		switch (version) {
+		
+		case 1:
+			toAdd = toAdd.replaceAll("[^,\\d]*", "");
+			numbers= toAdd.split(",");
+			if(numbers.length>0 && numbers[0].length()>0){
+				returnValue = Integer.valueOf(numbers[0]);
+			}
+			if (numbers.length>1){
+				returnValue += Integer.valueOf(numbers[1]);
+			}
+			return returnValue;
+		case 2:
+
+			toAdd = toAdd.replaceAll("[^,\\d]*", "");
+			numbers= toAdd.split(",");
+			
+			for ( String  number : numbers ) {
+				if(number.length()>0){
+					returnValue += Integer.valueOf(number);
+				}	
+			}
+			return returnValue;
+		case 3:
+
+			toAdd = toAdd.replaceAll("[^,\n\\d]*", "");
+			if(toAdd.matches(".*[,\n]{2,}.*")){
+				throw new IllegalArgumentException("consecutive separators are not allowed");
+			}
+			for (String stringNumber : toAdd.split(",") ) {
+				numbers = stringNumber.split("\n");
+				for ( String  number : numbers ) {
+					if(number.length()>0){
+						returnValue += Integer.valueOf(number);
+					}	
+				}
+			}
+
+			return returnValue;
+		case 4:
+			String numberString = toAdd.replaceAll("^//.\n", "");
+			String separator = toAdd.replaceFirst("\n.*$", "");
+			separator = separator.replaceFirst("^//", "");
+			if ( numberString.length()>0 && (numberString.matches(".*("+separator+"){2,}.*") || !numberString.matches("^\\d.*\\d$") )){
+				throw new IllegalArgumentException("the given string Does no match format: "+"\"//[delimiter]\\n[numbers…]\"");
+			}
+			numbers = numberString.split(separator);
+
+			for ( String  number : numbers ) {
+				if(number.length()>0 && number.matches("\\d+")){
+					returnValue += Integer.valueOf(number);
+				}else if(number.length()>0){
+					throw new IllegalArgumentException("The number: \""+number+"\" is not in the corret format");					
+				}
+			}
+			
+			return returnValue;
+		case 5:
+
+			String numbString = toAdd.replaceAll("^//.\n", "");
+			String separator2 = toAdd.replaceFirst("\n.*$", "");
+			separator2 = separator2.replaceFirst("^//", "");
+			if ( numbString.length()>0 && (numbString.matches(".*("+separator2+"){2,}.*") || !numbString.matches("^\\d.*\\d$") )){
+				throw new IllegalArgumentException("the given string Does no match format: "+"\"//[delimiter]\\n[numbers…]\"");
+			}
+			numbers = numbString.split(separator2);
+
+			for ( String  number : numbers ) {
+				if(number.length()>0 && number.matches("\\d+")){
+					returnValue += Integer.valueOf(number);
+				}else if(number.length()>0){
+					if(number.contains("-")){
+						throw new IllegalArgumentException("negative numbers are not allowed");					
+					}
+					throw new IllegalArgumentException("The number: \""+number+"\" is not in the corret format");					
+				}
+			}
+			
+			return returnValue;
+		default:
+			throw new IllegalArgumentException("Version: {"+version+"} is no suported");
+		}
+		
+	}
 }
